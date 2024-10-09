92964ac12ad18e0d23809a125807d814ea4b803f
IrisArias <ap18018@ues.edu.sv>
2024-10-08 19:04:44 -0600

    cambios agg

--- GestionCandidatosApi/obj/Debug/net8.0/GestionCandidatosApi.AssemblyInfo.cs
+++ GestionCandidatosApi/obj/Debug/net8.0/GestionCandidatosApi.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionCandidatosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022db67fa1a41b207fd69cd465f510a6a4d56f74")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5decd7d7df37b802a88b833749fed15be778004d")]
+>>>>>>> 3502102 (agregado de clases)
 [assembly: System.Reflection.AssemblyProductAttribute("GestionCandidatosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionCandidatosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
